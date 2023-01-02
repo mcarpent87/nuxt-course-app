fd68b6bc60ed754e797d0365888cbb7e9819967f
mcarpent87 <mclean.carpenter@lendio.com>
2023-01-01 22:03:14 -0700

    initial commit

---
+++ components/Navigation/TheHeader.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="header-container">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#"
+        ><nuxt-link to="/">CARPEMC BLOG</nuxt-link></b-navbar-brand
+      >
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <!-- <header class="the-header">
+    <TheSideNavToggle @toggle="$emit('sidenavToggle')" />
+    <div class="logo">
+      <nuxt-link to="/">CARPEMC BLOG</nuxt-link>
+    </div>
+    <div class="spacer"></div>
+    <div class="navigation-items">
+      <ul class="nav-list">
+        <li class="nav-item"><nuxt-link to="/posts">Blog</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/about">About</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/admin">Admin</nuxt-link></li>
+      </ul>
+    </div>
+  </header> -->
+  </div>
+</template>
+
+<script>
+import TheSideNavToggle from "@/components/Navigation/TheSideNavToggle";
+
+export default {
+  name: "TheHeader",
+  components: {
+    TheSideNavToggle,
+  },
+};
+</script>
+
+<style scoped>
+.header-container {
+  height: 60px;
+}
+
+.the-header {
+  width: 100%;
+  position: fixed;
+  height: 60px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: black;
+  z-index: 100;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+.logo {
+  margin: 0 10px;
+  font-size: 1.3rem;
+}
+
+.logo a {
+  text-decoration: none;
+  color: white;
+}
+
+.spacer {
+  flex: 1;
+}
+
+.navigation-items {
+  display: none;
+}
+
+@media (min-width: 768px) {
+  .navigation-items {
+    display: block;
+  }
+}
+
+.nav-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+}
+
+.nav-item {
+  margin: 0 10px;
+}
+
+.nav-item a {
+  text-decoration: none;
+  color: white;
+}
+
+.nav-item a:hover,
+.nav-item a:active,
+.nav-item a.nuxt-link-active {
+  color: red;
+}
+</style>
