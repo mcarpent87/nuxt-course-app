b299d8020f6ca51b8a21a6eeffbd497ffe749ee6
mcarpent87 <mclean.carpenter@lendio.com>
2023-01-04 21:10:42 -0700

    create vuex store

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
 <script>
-import PostList from '@/components/Posts/PostList'
+import PostList from "@/components/Posts/PostList";
 
 export default {
   components: {
-    PostList
-  }
-}
+    PostList,
+  },
+  asyncData(context, callback) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(null, {
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is our first post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is our second post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+            },
+          ],
+        });
+      }, 1000);
+    })
+    .then(data => {
+      return data
+    })
+    .catch((e) => {
+      context.error(new Error());
+    });
+  },
+};
 </script>
-
 
 <style scoped>
 .posts-page {
